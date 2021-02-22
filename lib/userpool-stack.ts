9134565b022abdc59c4ba2edfcf9c7175945ada0
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2021-02-22 21:21:52 +0500

    userpool

--- lib/userpool-stack.ts
+++ lib/userpool-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from '@aws-cdk/core';
+import * as cognito from '@aws-cdk/aws-cognito';
 
 export class UserpoolStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      selfSignUpEnabled: true, // Allow Users to Sign Up
+      autoVerify: {email: true}, // verify email address by sending a verification code
+      signInAliases: {email: true}, // Set email as an alias
+      userVerification: {
+        emailSubject: 'Verify your email for our awesome app!',
+        emailBody: 'Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}',
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+        smsMessage: 'Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}',
+      },
+
+    });
+
   }
 }
